aa7a54230c79cddc4bb95f7988baf63adbb32e97
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-10-14 16:59:54 -0500

    MATERIAL THEME IS HERE

--- html/index.js
+++ html/index.js
@@ -1,92 +1,90 @@
 const fs = require('fs')
 const os = require('os');
-const hiHome = os.userInfo().homedir+"\\AppData\\Local\\Temp\\Highlights";
-const viHome = os.userInfo().homedir+"\\Videos\\";
+const hiHome = os.userInfo().homedir + "\\AppData\\Local\\Temp\\Highlights";
+const viHome = os.userInfo().homedir + "\\Videos\\";
+let chips = document.getElementById("chips");
 let highlights = document.getElementById("highlights");
 let saving = document.getElementById("saving");
 let folders = [];
 
-if(!fs.existsSync(hiHome)) {
-    
-}
-
 reload()
 
 function saveClip(folder, file) {
-    highlights.style.display = "none";
+    chips.style.display = "none";
     saving.style.display = "block";
-    fs.rename(hiHome+"\\"+folder+"\\"+file, viHome+folder+"\\"+file, (e) => {})
-    highlights.style.display = "block";
+    fs.rename(hiHome + "\\" + folder + "\\" + file, viHome + folder + "\\" + file, (e) => { })
+    chips.style.display = "block";
     saving.style.display = "none";
     reload()
 }
 
 function saveAll(folder) {
-    fs.readdirSync(hiHome+"\\"+folder).forEach(file => {
-        highlights.style.display = "none";
+    fs.readdirSync(hiHome + "\\" + folder).forEach(file => {
+        chips.style.display = "none";
         saving.style.display = "block";
-        fs.rename(hiHome+"\\"+folder+"\\"+file, viHome+folder+"\\"+file, (e) => {})
-        highlights.style.display = "block";
+        fs.rename(hiHome + "\\" + folder + "\\" + file, viHome + folder + "\\" + file, (e) => { })
+        chips.style.display = "block";
         saving.style.display = "none";
         reload()
     })
 }
 
 function reload() {
-    reloadCss();
     let type = "";
-    if(type == "") {
-        highlights.innerHTML = "";
-    fs.readdirSync(hiHome).forEach(folder => {
-        if(fs.readdirSync(hiHome+"\\"+folder)) {
-            let details = document.createElement("details");
-            let summary = document.createElement("summary");
-            let saveAllButton = document.createElement("button");
-            saveAllButton.className = "btn";
-            saveAllButton.appendChild(document.createTextNode("Save All Clips"))
-            saveAllButton.onclick = () => {
-                saveAll(folder);
-            }
-            summary.appendChild(document.createTextNode(folder));
-            details.appendChild(summary)
-            details.id = folder;
-            details.appendChild(saveAllButton)
-            highlights.appendChild(details)
-            fs.readdirSync(hiHome+"\\"+folder).forEach(file => {
-                let p = document.createElement("p");
-                p.id = file;
-                let btn = document.createElement("button");
-                btn.appendChild(document.createTextNode("Save Clip"));
-                btn.onclick = () => {
-                    saveClip(folder, file)
+    if (type == "") {
+        chips.innerHTML = "";
+        fs.readdirSync(hiHome).forEach(folder => {
+            if (fs.readdirSync(hiHome + "\\" + folder)) {
+                let div = document.createElement("div");
+                div.className = "chip highlightChip z-depth-2 noselect";
+                div.innerText = folder;
+                div.onclick = e => {
+                    selectHighlights(folder);
                 }
-                p.appendChild(document.createTextNode(file+" "))
-                p.appendChild(btn)
-                document.getElementById(folder).appendChild(p)
-            })
-        }
-    })
+                chips.appendChild(div)
+            }
+        })
     }
 }
 
-function isDirEmpty(dirname) {
-    return fs.promises.readdir(dirname).then(files => {
-        return files.length === 0;
-    });
-}
-
-if(highlights.innerHTML == "") {
+if (chips.innerHTML == "") {
     saving.innerText = "Go capture some highlights, you don't have any unsaved ones!";
     saving.style.display = "block"
 }
 
-function reloadCss()
-{
+function reloadCss() {
     var links = document.getElementsByTagName("link");
-    for (var cl in links)
-    {
+    for (var cl in links) {
         var link = links[cl];
         if (link.rel === "stylesheet")
             link.href += "";
     }
+}
+
+function selectHighlights(folder) {
+    highlights.innerHTML = "";
+    let saveAllButton = document.createElement("button");
+    saveAllButton.className = "btn";
+    saveAllButton.appendChild(document.createTextNode("Save All Clips"))
+    saveAllButton.onclick = () => {
+        saveAll(folder);
+    }
+    highlights.append(saveAllButton)
+    fs.readdirSync(hiHome + "\\" + folder).forEach(file => {
+        let p = document.createElement("p");
+        p.id = file;
+        p.className = "card-panel teal lighten-2";
+        p.appendChild(document.createTextNode(file + " "))
+        let btn = document.createElement("button");
+        btn.appendChild(document.createTextNode("Save Clip"));
+        btn.className = "btn"
+        btn.onclick = () => {
+            saving.innerText = "Saving clip!";
+            saving.style.display = "block"
+            saveClip(folder, file)
+        }
+        p.appendChild(btn)
+        highlights.appendChild(p)
+        saving.style.display = "none"
+    })
 }
