aedc9c759ae6b865a1ce1e8ffdbb0989355a4b13
Detroit Man <101349397+sancheza2670@users.noreply.github.com>
2022-10-13 21:17:50 -0500

    no longer need js for 100% worky

--- html/index.js
+++ html/index.js
@@ -1,41 +1,39 @@
 const fs = require('fs')
 const os = require('os');
 const hiHome = os.userInfo().homedir+"\\AppData\\Local\\Temp\\Highlights";
-let highlights;
+let highlights = document.getElementById("highlights");
 let folders = [];
 
-setup();
-
 if(!fs.existsSync(hiHome)) {
-    document.getElementById("noshadowplay").style.visibility = "visible";
+    
 }
 
 reload()
 
 function saveClip(folder, file) {
     console.log("Saving "+hiHome+"\\"+folder+"\\"+file)
-    highlights.style.visibility = "hidden";
-    document.getElementById("saving").style.visibility = "visible";
+    highlights.style.display = "none";
+    document.getElementById("saving").style.display = "block";
     fs.rename(hiHome+"\\"+folder+"\\"+file, os.userInfo().homedir+"\\Videos\\"+folder+"\\"+file, (e) => {
         console.log(e)
     })
-    highlights.style.visibility = "visible";
-    document.getElementById("saving").style.visibility = "hidden";
+    highlights.style.display = "block";
+    document.getElementById("saving").style.display = "none";
     reload()
 }
 
 function saveAll(folder) {
     fs.readdirSync(hiHome+"\\"+folder).forEach(file => {
         console.log("Saving "+hiHome+"\\"+folder+"\\"+file)
-        highlights.style.visibility = "hidden";
-        document.getElementById("saving").style.visibility = "visible";
+        highlights.style.display = "none";
+        document.getElementById("saving").style.display = "block";
         fs.rename(hiHome+"\\"+folder+"\\"+file, os.userInfo().homedir+"\\Videos\\"+folder+"\\"+file, (e) => {
             if(e) {
                 console.log(e)
             }
         })
-        highlights.style.visibility = "visible";
-        document.getElementById("saving").style.visibility = "hidden";
+        highlights.style.display = "block";
+        document.getElementById("saving").style.display = "none";
         reload()
     })
 }
@@ -83,22 +81,5 @@ function isDirEmpty(dirname) {
 
 if(highlights.innerHTML == "") {
     document.getElementById("saving").innerText = "Go capture some highlights, you don't have any unsaved ones!";
-    document.getElementById("saving").style.visibility = "visible"
-}
-
-function setup() {
-    document.getElementById("noshadowplay").innerText = "This tool only works with NVIDIA Shadowplay. You either have 0 unsaved highlights, GeForce Experience is not installed, the app does not have admin rights, or your temporary Highlights folder is missing!";
-    document.getElementById("noshadowplay").style.visibility = "hidden";
-    let a = document.createElement("h1")
-    let b = document.createElement("h2");
-    let c = document.createElement('div');
-    c.id = "highlights";
-    b.appendChild(document.createTextNode("Saving clip..."));
-    b.id = "saving";
-    b.style.visibility = "hidden";
-    a.appendChild(document.createTextNode("Extra Highlights"));
-    document.body.appendChild(a)
-    document.body.appendChild(b)
-    document.body.appendChild(c)
-    highlights = document.getElementById("highlights");
+    document.getElementById("saving").style.display = "block"
 }
